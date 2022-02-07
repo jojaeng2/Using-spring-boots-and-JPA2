461f7202cc0c7c57a69ee10bd0eaac408dc283d9
jojaeng2 <ds4ouj@naver.com>
2022-02-07 11:15:15 +0900

    setting Environment

---
+++ jpashop/src/main/java/jpabook/jpashop/Service/ItemService.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop.Service;
+
+import jpabook.jpashop.domain.Book;
+import jpabook.jpashop.domain.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    @Transactional
+    public void update(Long itemId, int price, String name, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
